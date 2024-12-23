05d0563398930e0b7590e7cea7010d4053c63526
ayishahudakt <24mcag01@meaec.edu.in>
2024-12-23 11:17:35 +0530

    user input

--- pgm2.py
+++ pgm2.py
@@ -1,7 +1,9 @@
-a=10
-b=2.5
-c=4.05j
-print("a is type of",type(a))
-print("b is type of",type(b))
-print("c is type of",type(c))
+a=eval(input("enter a first number:"))
+b=eval(input("enter a second number:"))
+c=eval(input("enter a third number:"))
+print("the types of the numbers are......!!!!")
+print("first number type",type(a))
+print("second number type",type(b))
+print("third numbe type",type(c))
+
 
